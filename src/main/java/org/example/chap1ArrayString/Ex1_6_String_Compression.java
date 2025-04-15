684fff85f860f8976463b8871fafcd1dd9cd62f0
Minh Nhat Vu <minhnhatse.uit@gmail.com>
2025-04-15 17:42:50 +0100

    443. String Compression and 3163. String Compression III

--- src/main/java/org/example/chap1ArrayString/Ex1_6_String_Compression.java
+++ src/main/java/org/example/chap1ArrayString/Ex1_6_String_Compression.java
@@ -2,6 +2,8 @@ package org.example.chap1ArrayString;
 
 import org.junit.jupiter.api.Assertions;
 
+import java.util.Arrays;
+
 public class Ex1_6_String_Compression{
     /*
         Implement a method to perform basic string compression using the counts
@@ -13,9 +15,39 @@ the original string.
 
 You can assume the string has only uppercase and lowercase letters (a - z).
          */
+
+    /*
+    https://leetcode.com/problems/string-compression/description/
+     */
     public static void main(String[] args) {
         Assertions.assertEquals("a2b1c5a3", compress("aabcccccaaa"));
         Assertions.assertEquals("a", compress("a"));
+
+
+        char[] input;
+        int length;
+
+//        input = new char[] {'a','a','b','b','c','c','c'};
+//        length = compress_letcode_443(input);
+//        System.out.println("Length: " + length);
+//        System.out.println("output: " + Arrays.toString(input));
+//        System.out.println("\n");
+
+//        input = new char[] {'a'};
+//        length = compress_letcode_443(input);
+//        System.out.println("Length: " + length);
+//        System.out.println("output: " + Arrays.toString(input));
+
+        String[] strArr = {"a","b","b","b","b","b","b","b","b","b","b","b","b"};
+        input = new char[strArr.length];
+        for (int i = 0; i < strArr.length; i++){
+            input[i] = strArr[i].charAt(0);
+        }
+        length = compress_letcode_443(input);
+        System.out.println("Length: " + length);
+        System.out.println("output: " + Arrays.toString(input)); // ab12
+
+
     }
 
     public static String compress(String origin){
@@ -43,5 +75,66 @@ You can assume the string has only uppercase and lowercase letters (a - z).
         sb.append(count);
 
         return sb.length() < origin.length() ? sb.toString() : origin;
+    }
+
+    // https://leetcode.com/problems/string-compression/description/
+    public static int compress_letcode_443(char[] chars) {
+        int read = 0; // possition to read from the array
+        int write = 0; // possition to write to the array
+
+        while(read < chars.length){
+            char currChar = chars[read];
+            int count = 0;
+
+            // Count occurrences of the current character
+            while(read < chars.length && chars[read] == currChar){
+                count++;
+                read++;
+            }
+
+            // Write the character
+            chars[write++] = currChar;
+
+            if(count > 1){
+                for(char c: String.valueOf(count).toCharArray()){
+                    chars[write++] = c;
+                }
+            }
+        }
+        return write;
+    }
+
+    // 3163. String Compression III
+    public String compressedString_III(String word) {
+        if(word.isEmpty()){
+            return "";
+        }
+
+        int read = 0; // read possition from word
+        int write = 0; // write possition from word
+        StringBuilder sb = new StringBuilder();
+
+        while(read < word.length()){
+            char currChar = word.charAt(read);
+            int count = 0;
+
+            // count consecutive repeated letter
+            while(read < word.length() && word.charAt(read) == currChar){
+                count++;
+                read++;
+            }
+
+            // build result string
+            while(count > 0){
+                int occurNum = Math.min(count, 9);
+                sb.append(occurNum);
+                sb.append(currChar);
+
+                count -= occurNum;
+            }
+        }
+
+        return sb.toString();
+
     }
 }
